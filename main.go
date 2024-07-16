68f82012ed885b881bc794250cdf6563d5eec01f
Karel Najman <halymist@gmail.com>
2024-07-16 16:30:49 +0200

    test

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "test test!",
+		Body:       "test test test!",
 		Headers: map[string]string{
 			"Content-Type": "text/plain",
 		},
