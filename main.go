a68a5f0b329b2c014c0a995e8d50ae2ac94a752e
Karel Najman <halymist@gmail.com>
2024-07-16 18:35:32 +0200

    for franta

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "test test test!",
+		Body:       "franta smrdí!",
 		Headers: map[string]string{
 			"Content-Type": "text/plain",
 		},
