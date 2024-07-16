add7ca57f4ab5fba4d556d56fc383e0276f5e486
Karel Najman <halymist@gmail.com>
2024-07-16 15:00:03 +0200

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("ahoj")
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+		Headers: map[string]string{
+			"Content-Type": "text/plain",
+		},
+	}
+
+	return response, nil
+}
